8e1bbf4b9f237a27e689d379a1d3aebbab16f05d
HexNowLoading <dungeon.nowloading@gmail.com>
2024-06-05 09:45:02 -0700

    Redstone Lane Done

--- common/src/main/java/dev/hexnowloading/dungeonnowloading/block/RedstoneLaneBlock.java
+++ common/src/main/java/dev/hexnowloading/dungeonnowloading/block/RedstoneLaneBlock.java
@@ -5,9 +5,14 @@ import dev.hexnowloading.dungeonnowloading.registry.DNLBlocks;
 import dev.hexnowloading.dungeonnowloading.registry.DNLProperties;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.core.particles.DustParticleOptions;
+import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.RandomSource;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.item.enchantment.EnchantmentHelper;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.state.BlockState;
@@ -55,15 +60,28 @@ public class RedstoneLaneBlock extends DirectionalBlock {
     }
 
     @Override
+    public void stepOn(Level level, BlockPos blockPos, BlockState blockState, Entity entity) {
+        if (level.getBlockState(blockPos).getValue(DNLProperties.REDSTONE_LANE_MODE) == RedstoneLaneMode.OVERPOWERED) {
+            if (entity instanceof LivingEntity && !EnchantmentHelper.hasFrostWalker((LivingEntity) entity)) {
+                entity.hurt(level.damageSources().hotFloor(), 6.0F);
+                if (!entity.fireImmune()) {
+                    entity.setSecondsOnFire(5);
+                }
+            }
+        }
+        super.stepOn(level, blockPos, blockState, entity);
+    }
+
+    @Override
     public void onPlace(BlockState blockState, Level level, BlockPos blockPos, BlockState oldBlockState, boolean b) {
         updatePowerStrength(blockState, level, blockPos);
         super.onPlace(blockState, level, blockPos, oldBlockState, b);
     }
 
-    @Override
+    /*@Override
     public void tick(BlockState blockState, ServerLevel serverLevel, BlockPos blockPos, RandomSource randomSource) {
         serverLevel.setBlock(blockPos, blockState.setValue(DNLProperties.REDSTONE_LANE_MODE, RedstoneLaneMode.POWERED), 2);
-    }
+    }*/
 
     @Override
     public void neighborChanged(BlockState blockState, Level level, BlockPos blockPos, Block block, BlockPos neighbourBlock, boolean b) {
@@ -71,7 +89,19 @@ public class RedstoneLaneBlock extends DirectionalBlock {
         if (blockState.getValue(REDSTONE_LANE_MODE) == RedstoneLaneMode.UNPOWERED) {
             if (level.getBlockState(neighbourBlock).is(Blocks.REDSTONE_BLOCK)) {
                 neighbourChangedRedstoneBlock(blockState, level, blockPos, block, neighbourBlock);
-            } else if ((level.getBlockState(neighbourBlock).is(DNLBlocks.REDSTONE_LANE_I.get()) || level.getBlockState(neighbourBlock).is(DNLBlocks.REDSTONE_LANE_L.get()) || level.getBlockState(neighbourBlock).is(DNLBlocks.REDSTONE_LANE_T.get())) && level.getBlockState(neighbourBlock).getValue(REDSTONE_LANE_MODE) == RedstoneLaneMode.POWERED) {
+            } else if (level.getBlockState(neighbourBlock).is(Blocks.MAGMA_BLOCK)) {
+                neighbourChangedOverpoweredBlock(blockState, level, blockPos, block, neighbourBlock);
+            } else if (
+                    (
+                            level.getBlockState(neighbourBlock).is(DNLBlocks.REDSTONE_LANE_I.get())
+                            || level.getBlockState(neighbourBlock).is(DNLBlocks.REDSTONE_LANE_L.get())
+                            || level.getBlockState(neighbourBlock).is(DNLBlocks.REDSTONE_LANE_T.get())
+                    )
+                    && (
+                            level.getBlockState(neighbourBlock).getValue(REDSTONE_LANE_MODE) == RedstoneLaneMode.POWERED
+                            || level.getBlockState(neighbourBlock).getValue(REDSTONE_LANE_MODE) == RedstoneLaneMode.OVERPOWERED
+                    )
+            ) {
                 neighbourChangedLane(blockState, level, blockPos, block, neighbourBlock);
             }
         } else {
@@ -85,11 +115,20 @@ public class RedstoneLaneBlock extends DirectionalBlock {
 
         int power;
 
+        boolean overpowered = false;
+
         boolean hasRedstoneBlock = !neighborLaneBlockPosList.stream().filter(b -> level.getBlockState(b).is(Blocks.REDSTONE_BLOCK)).toList().isEmpty();
 
-        if (hasRedstoneBlock) {
+        boolean hasMagmaBlock = !neighborLaneBlockPosList.stream().filter(b -> level.getBlockState(b).is(Blocks.MAGMA_BLOCK)).toList().isEmpty();
+
+        if (hasRedstoneBlock || level.getBlockState(blockPos.above()).is(Blocks.REDSTONE_BLOCK)) {
 
             power = 15;
+
+        } else if (hasMagmaBlock || level.getBlockState(blockPos.above()).is(Blocks.MAGMA_BLOCK)) {
+
+            power = 15;
+            overpowered = true;
 
         } else {
 
@@ -102,6 +141,8 @@ public class RedstoneLaneBlock extends DirectionalBlock {
 
             int highestPower = redstoneLanePosList.stream().mapToInt(b -> level.getBlockState(b).getValue(DNLProperties.REDSTONE_LANE_POWER)).max().orElse(0);
 
+            overpowered = !redstoneLanePosList.stream().filter(b -> level.getBlockState(b).getValue(DNLProperties.REDSTONE_LANE_MODE) == RedstoneLaneMode.OVERPOWERED).toList().isEmpty();
+
             power = Math.max(highestPower - 1, 0);
 
             if (originalPower == power) return;
@@ -110,7 +151,11 @@ public class RedstoneLaneBlock extends DirectionalBlock {
         if (power == 0) {
             level.setBlock(blockPos, blockState.setValue(DNLProperties.REDSTONE_LANE_MODE, RedstoneLaneMode.UNPOWERED).setValue(DNLProperties.REDSTONE_LANE_POWER, 0), 2);
         } else {
-            level.setBlock(blockPos, blockState.setValue(DNLProperties.REDSTONE_LANE_MODE, RedstoneLaneMode.POWERED).setValue(DNLProperties.REDSTONE_LANE_POWER, power), 2);
+            if (overpowered) {
+                level.setBlock(blockPos, blockState.setValue(DNLProperties.REDSTONE_LANE_MODE, RedstoneLaneMode.OVERPOWERED).setValue(DNLProperties.REDSTONE_LANE_POWER, power), 2);
+            } else {
+                level.setBlock(blockPos, blockState.setValue(DNLProperties.REDSTONE_LANE_MODE, RedstoneLaneMode.POWERED).setValue(DNLProperties.REDSTONE_LANE_POWER, power), 2);
+            }
         }
 
         updateConnectedNegihbors(neighborLaneBlockPosList, level, blockPos);
@@ -119,6 +164,8 @@ public class RedstoneLaneBlock extends DirectionalBlock {
     private void neighbourChangedRedstoneBlock(BlockState blockState, Level level, BlockPos blockPos, Block block, BlockPos neighbourBlock) {
 
         List<BlockPos> neighborBlockPosList = getConnectionBlockPos(blockPos, blockState);
+
+        neighborBlockPosList.add(blockPos.above());
 
         boolean isRedstoneBlock = false;
 
@@ -131,6 +178,30 @@ public class RedstoneLaneBlock extends DirectionalBlock {
 
         level.setBlock(blockPos, blockState.setValue(DNLProperties.REDSTONE_LANE_MODE, RedstoneLaneMode.POWERED).setValue(DNLProperties.REDSTONE_LANE_POWER, 15), 2);
 
+        poweredParticle(level, blockState, blockPos);
+
+        updateConnectedNeighborsWithExcluded(level, blockState, blockPos, neighbourBlock);
+    }
+
+    private void neighbourChangedOverpoweredBlock(BlockState blockState, Level level, BlockPos blockPos, Block block, BlockPos neighbourBlock) {
+
+        List<BlockPos> neighborBlockPosList = getConnectionBlockPos(blockPos, blockState);
+
+        neighborBlockPosList.add(blockPos.above());
+
+        boolean isOverpoweredBlock = false;
+
+        for (BlockPos pos : neighborBlockPosList) {
+            isOverpoweredBlock = pos.equals(neighbourBlock);
+            if (isOverpoweredBlock) break;
+        }
+
+        if (!isOverpoweredBlock) return;
+
+        level.setBlock(blockPos, blockState.setValue(DNLProperties.REDSTONE_LANE_MODE, RedstoneLaneMode.OVERPOWERED).setValue(DNLProperties.REDSTONE_LANE_POWER, 15), 2);
+
+        poweredParticle(level, blockState, blockPos);
+
         updateConnectedNeighborsWithExcluded(level, blockState, blockPos, neighbourBlock);
     }
 
@@ -140,10 +211,17 @@ public class RedstoneLaneBlock extends DirectionalBlock {
 
         int power = level.getBlockState(neighbourBlock).getValue(DNLProperties.REDSTONE_LANE_POWER) - 1;
 
+        boolean overpowered = level.getBlockState(neighbourBlock).getValue(DNLProperties.REDSTONE_LANE_MODE) == RedstoneLaneMode.OVERPOWERED;
+
         if (power == 0) {
             level.setBlock(blockPos, blockState.setValue(DNLProperties.REDSTONE_LANE_MODE, RedstoneLaneMode.UNPOWERED).setValue(DNLProperties.REDSTONE_LANE_POWER, 0), 2);
         } else {
-            level.setBlock(blockPos, blockState.setValue(DNLProperties.REDSTONE_LANE_MODE, RedstoneLaneMode.POWERED).setValue(DNLProperties.REDSTONE_LANE_POWER, power), 2);
+            if (overpowered) {
+                level.setBlock(blockPos, blockState.setValue(DNLProperties.REDSTONE_LANE_MODE, RedstoneLaneMode.OVERPOWERED).setValue(DNLProperties.REDSTONE_LANE_POWER, power), 2);
+            } else {
+                level.setBlock(blockPos, blockState.setValue(DNLProperties.REDSTONE_LANE_MODE, RedstoneLaneMode.POWERED).setValue(DNLProperties.REDSTONE_LANE_POWER, power), 2);
+            }
+            poweredParticle(level, blockState, blockPos);
         }
 
         updateConnectedNeighborsWithExcluded(level, blockState, blockPos, neighbourBlock);
@@ -210,5 +288,53 @@ public class RedstoneLaneBlock extends DirectionalBlock {
         }
 
         return neighborBlockPosList;
+    }
+
+    @Override
+    public void animateTick(BlockState blockState, Level level, BlockPos blockPos, RandomSource randomSource) {
+        switch (blockState.getValue(DNLProperties.REDSTONE_LANE_MODE)) {
+            default:
+            case UNPOWERED:
+                break;
+            case POWERED:
+                if (randomSource.nextFloat() > 0.7F) {
+                    float r = randomSource.nextFloat() - randomSource.nextFloat();
+                    double x = (double)blockPos.getX() + randomSource.nextFloat();
+                    double y = (double)blockPos.getY() + 1.1 + 0.1 * r;
+                    double z = (double)blockPos.getZ() + randomSource.nextFloat();
+                    level.addParticle(DustParticleOptions.REDSTONE, x, y, z, 0.0D, 0.0D, 0.0D);
+                }
+                break;
+            case OVERPOWERED:
+                if (randomSource.nextFloat() > 0.7F) {
+                    float r = randomSource.nextFloat() - randomSource.nextFloat();
+                    double x = (double)blockPos.getX() + randomSource.nextFloat();
+                    double y = (double)blockPos.getY() + 1.1 + 0.1 * r;
+                    double z = (double)blockPos.getZ() + randomSource.nextFloat();
+                    level.addParticle(DustParticleOptions.REDSTONE, x, y, z, 0.0D, 0.0D, 0.0D);
+                }
+                if (randomSource.nextFloat() > 0.9F) {
+                    float r = randomSource.nextFloat() - randomSource.nextFloat();
+                    double x = (double)blockPos.getX() + randomSource.nextFloat();
+                    double y = (double)blockPos.getY() + 1.1 + 0.1 * r;
+                    double z = (double)blockPos.getZ() + randomSource.nextFloat();
+                    level.addParticle(ParticleTypes.FLAME, x, y, z, 0.0D, 0.0D, 0.0D);
+                }
+                break;
+        }
+        if (blockState.getValue(DNLProperties.REDSTONE_LANE_MODE) == RedstoneLaneMode.POWERED && randomSource.nextFloat() > 0.5F) {
+            float r = randomSource.nextFloat() - randomSource.nextFloat();
+            double x = (double)blockPos.getX() + randomSource.nextFloat();
+            double y = (double)blockPos.getY() + 1.1 + 0.1 * r;
+            double z = (double)blockPos.getZ() + randomSource.nextFloat();
+            level.addParticle(DustParticleOptions.REDSTONE, x, y, z, 0.0D, 0.0D, 0.0D);
+        }
+    }
+
+    private void poweredParticle(Level level, BlockState blockState, BlockPos blockPos) {
+        double x = (double)blockPos.getX() + 0.5;
+        double y = (double)blockPos.getY() + 1.1;
+        double z = (double)blockPos.getZ() + 0.5;
+        ((ServerLevel) level).sendParticles(DustParticleOptions.REDSTONE, x, y, z, 5, 0.25D, 0.1D, 0.25D, 0.0);
     }
 }
